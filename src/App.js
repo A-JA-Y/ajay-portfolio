b450be86327be31f465e4fdf6b6e32b8f94b5a6f
Ajay <mailtoajay7007@gmail.com>
2024-07-25 23:20:03 +0530

    Navbar - non responsive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import Typed from 'typed.js';
-import React from'react';
+import React from "react";
+import Navbar from "./components/navbar";
 
 function App() {
-  const el = React.useRef(null);
-
-  React.useEffect(() => {
-    const typed = new Typed(el.current, {
-      strings: ['<i>Portfolio</i> website.',],
-      typeSpeed: 50,
-    });
-
-    return () => {
-      // Destroy Typed instance during cleanup to stop animation
-      typed.destroy();
-    };
-  }, []);
   return (
     <>
-    <h1 className='text-7xl font-extrabold '>This is my <span ref={el} /></h1>
-
-
+      <Navbar />
     </>
   );
 }
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from "react";
+import Typed from "typed.js";
 
 export default function Navbar() {
+  const el = React.useRef(null);
+
+  React.useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["<i>Ajay.</i>"],
+      typeSpeed: 50,
+      showCursor: false,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
   return (
     <>
-    
+      <nav className="min-w-full  bg-transparent dark:text-white dark:bg-gray-950">
+        <div className="navItems flex items-center justify-center max-w-6xl mx-auto px-4">
+          <div className="logo">
+            <h1
+              className="text-4xl font-bold m-2 font-mono mx-4 transition-all hover:cursor-none "
+              ref={el}
+            ></h1>
+          </div>
+          <div className="navLinks">
+            <ul className="flex justify-end">
+              <li className="m-2 text-xl font-mono hover:cursor-pointer hover:text-gray-600 transition-all">
+                About
+              </li>
+              <li className="m-2 text-xl font-mono hover:cursor-pointer hover:text-gray-600 transition-all">
+                Projects
+              </li>
+              <li className="m-2 text-xl font-mono hover:cursor-pointer hover:text-gray-600 transition-all">
+                Contact
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* <div className="mobile md:hidden">
+          <div className="logo">
+            <h1
+              className="text-4xl font-bold m-2 font-mono mx-4 transition-all hover:cursor-none "
+              ref={el}
+            ></h1>
+          </div>
+          <div className="navLinks">
+            <ul className="flex justify-end">
+              <li className="m-2 text-xl font-mono hover:cursor-pointer hover:text-gray-600 transition-all">
+                About
+              </li>
+              <li className="m-2 text-xl font-mono hover:cursor-pointer hover:text-gray-600 transition-all">
+                Projects
+              </li>
+              <li className="m-2 text-xl font-mono hover:cursor-pointer hover:text-gray-600 transition-all">
+                Contact
+              </li>
+            </ul>
+          </div> */}
+        
+        
+      </nav>
     </>
-  )
+  );
 }
